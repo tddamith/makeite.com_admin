351904bd348c79e815e333b3e672f9d62c0c9465
damith <dpremakumara@gmail.com>
2025-11-03 10:25:50 +0530

    test tool

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Editor from "./Editor";
+import Editor2 from "./Editor_2";
+import Editor3 from "./Editor_3";
+
+function App() {
+  return (
+      <div style={{ padding: "2rem" }}>
+        <h1>🎨 Invitation Editor</h1>
+        {/*<Editor />*/}
+        <Editor3 />
+      </div>
+  );
+}
+
+export default App;
