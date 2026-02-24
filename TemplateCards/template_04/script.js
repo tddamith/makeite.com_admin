dea40d73aacd07968dacef609630282a02e9f574
Dushani96 <168927373+Dushani96@users.noreply.github.com>
2026-02-24 11:49:05 +0530

    update

--- TemplateCards/template_04/script.js
+++
@@ -1,8 +0,0 @@
-// Example: Add simple fade-in effect on load
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".invitation-card").style.opacity = 0;
-  setTimeout(() => {
-    document.querySelector(".invitation-card").style.transition = "1.5s";
-    document.querySelector(".invitation-card").style.opacity = 1;
-  }, 200);
-});
